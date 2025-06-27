2fc823bb856c0defcf1b0f76bef53ed25010ceb4
Tejas Devendra Devgharkar <tejasdevgharkar12@gmail.com>
2025-06-27 10:02:03 +0530

    Initial Commit

---
+++ NetMaxx/src/main/java/com/netMaxx/NetMaxx/NetMaxxApplication.java
@@ -0,0 +1,13 @@
+package com.netMaxx.NetMaxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetMaxxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetMaxxApplication.class, args);
+	}
+
+}
---
+++ NetMaxx/src/test/java/com/netMaxx/NetMaxx/NetMaxxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netMaxx.NetMaxx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetMaxxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
